52fc25344d67d35f788cf50d7b8737cb1f6a71da
long1712578 <pdlong578@gmail.com>
2021-02-02 10:34:06 +0700

    commit 2

--- script.js
+++ script.js
@@ -1,25 +1,8 @@
-ready(function () {
-  initWidget();
-});
-
-function ready(callbackFunction) {
-  if (document.readyState != 'loading')
-      callbackFunction()
-  else
-      document.addEventListener("DOMContentLoaded", callbackFunction)
-}
-
-function loadIframe() {
-
-  var containerDiv = document.createElement('div');
-  containerDiv.setAttribute('id', 'tile-container');
-  containerDiv.setAttribute('style', 'display: block !important;z-index: 2000000000!important;right: 10px!important;bottom: 0px!important;left: auto!important;position: fixed!important;');
-  document.body.appendChild(containerDiv);
-  var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("frameborder", "0");
-  ifrm.setAttribute("border", "0");
-  ifrm.setAttribute("id", "messageId");
-  ifrm.setAttribute("style", `outline: none !important;
+ready(function(){initWidget();});function ready(callbackFunction){if(document.readyState!='loading')
+callbackFunction()
+else
+document.addEventListener("DOMContentLoaded",callbackFunction)}
+function loadIframe(){var containerDiv=document.createElement('div');containerDiv.setAttribute('id','tile-container');containerDiv.setAttribute('style','display: block !important;z-index: 2000000000!important;right: 10px!important;bottom: 0px!important;left: auto!important;position: fixed!important;');document.body.appendChild(containerDiv);var ifrm=document.createElement("iframe");ifrm.setAttribute("frameborder","0");ifrm.setAttribute("border","0");ifrm.setAttribute("id","messageId");ifrm.setAttribute("style",`outline: none !important;
     visibility: visible !important;
       resize: none !important;
       box-shadow: 
@@ -35,25 +18,43 @@ function loadIframe() {
       width: 300px !important; z-index: 999999 !important; cursor: auto !important; 
       float: none !important; border-radius: unset !important; pointer-events: auto !important;
       display: block !important; height: 400px !important;`);
-
-  var htmlMessage = '<html lang="en">';
-  htmlMessage += '<head>';
-  htmlMessage += '<meta charset="utf-8">';
-  htmlMessage += '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />';
-  htmlMessage += '<title>Tilechat Widget</title>';
-  htmlMessage += '<base href="./">';
-  htmlMessage += '<link rel="icon" type="image/x-icon" href="favicon.ico">';
-  htmlMessage+='<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">';
-  htmlMessage+='<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>';
-  htmlMessage += `<script>
-    function moForm() {
+      var htmlMessage='<html lang="en">';
+      htmlMessage+='<head>';
+      htmlMessage+='<meta charset="utf-8">';
+      htmlMessage+='<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />';
+      htmlMessage+='<title>Tilechat Widget</title>';
+      htmlMessage+='<base href="./">';
+      htmlMessage+='<link rel="icon" type="image/x-icon" href="favicon.ico">';
+      htmlMessage+='<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">';
+      htmlMessage+='<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>';
+      htmlMessage+='<script src="https://cdn.socket.io/socket.io-3.0.1.min.js"></script>';
+      htmlMessage+=`<script>
+      function moForm() {
         document.getElementById("myForm").style.display = "block";
       };
       function dongForm() {
         document.getElementById("myForm").style.display = "none";
       };
+      var connectionOptions =  {
+        "force new connection" : true,
+        "reconnectionAttempts": "Infinity", 
+        "timeout" : 10000,                  
+        "transports" : ["websocket"]
+        };
+      var socket = io.connect("http://localhost:9000",connectionOptions);
+      function guiTenForm(){
+        var message=document.getElementById('message').value;
+        
+          socket.emit("user_join", message);
+          document.getElementById('message').value="";
+      };
+      function guiMsgForm(){
+        var message=document.getElementById('message').value;
+        socket.emit("send_message", message);
+          document.getElementById('message').value="";
+      };
     </script>`;
-  htmlMessage += `<style>
+    htmlMessage+=`<style>
     {box-sizing: border-box;}
 
       .nut-mo-chatbox {
@@ -120,10 +121,7 @@ function loadIframe() {
       .form-container .btn:hover, .nut-mo-chatbox:hover {
           opacity: 1;
       }
-    </style>`;
-  htmlMessage += '</head>';
-  htmlMessage += '<body>';
-  htmlMessage += `<button class="nut-mo-chatbox" onclick="moForm()"><i class="fa fa-comments"></i></button>
+    </style>`;htmlMessage+='</head>';htmlMessage+='<body>';htmlMessage+=`<button class="nut-mo-chatbox" onclick="moForm()"><i class="fa fa-comments"></i></button>
     <div class="Chatbox" id="myForm">
       <form  class="form-container">
         <h1>Chatbox</h1>
@@ -131,42 +129,12 @@ function loadIframe() {
         <label for="msg"><b>Lời Nhắn</b></label>
         <textarea placeholder="Bạn hãy nhập lời nhắn.." name="msg" id="message" required></textarea>
     
-        <button type="submit"  class="btn send-message" >Gửi</button>
+        <button type="button"  onclick="guiMsgForm()" class="btn" >Gửi msg</button>
+        <button type="button"  onclick="guiTenForm()" class="btn" >Gửi tên</button>
         <button type="button" class="btn nut-dong-chatbox" onclick="dongForm()">Đóng</button>
       </form>
     </div>
-    <script src="https://cdn.socket.io/socket.io-3.0.1.min.js"></script>
-    <script>
-      $(document).ready(()=>{
-        $('.send-message').on('click', sendMessage);
-      });
-      function sendMessage(){
-        var message=document.getElementById('message').value;
-        console.log("message",message);
-        var connectionOptions =  {
-          "force new connection" : true,
-          "reconnectionAttempts": "Infinity", 
-          "timeout" : 10000,                  
-          "transports" : ["websocket"]
-      };
-        var socket = io.connect("http://localhost:9000",connectionOptions);
-        console.log("connect ",socket);
-        socket.on("connect", function() {
-          console.log("user_join");
-          socket.emit("user_join", message);
-        });
-      }
-    </script>
 </body>
 </html>
-`;
-  containerDiv.appendChild(ifrm);
-  ifrm.contentWindow.document.open();
-  ifrm.contentWindow.document.write(htmlMessage);
-  ifrm.contentWindow.document.close();
-
-
-}
-function initWidget() {
-  loadIframe();
-}
+`;containerDiv.appendChild(ifrm);ifrm.contentWindow.document.open();ifrm.contentWindow.document.write(htmlMessage);ifrm.contentWindow.document.close();}
+function initWidget(){loadIframe();}
